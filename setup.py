ecbd569921de4038cfe1b77262d2ab43c91a99c4
Peter Hill <zed.three@gmail.com>
2020-02-26 13:29:11 +0000

    Add python 3.6, 3.7 to setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Physics",
         "Topic :: Scientific/Engineering :: Visualization",
     ],
