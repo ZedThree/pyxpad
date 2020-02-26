c6a48b2d2dd6b5bd71a1e797830abb1a98e54680
Peter Hill <zed.three@gmail.com>
2020-02-26 13:27:50 +0000

    Remove unused import; black-format __main__

--- pyxpad/__main__.py
+++ pyxpad/__main__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 from pyxpad import PyXPad
 from Qt.QtWidgets import QApplication
 import sys
@@ -19,11 +18,19 @@ def main():
     """
     # Add command line arguments
     parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument("-c", "--config", nargs=1, help="Config file to load",
-                        default=None)
-    parser.add_argument("-i", "--ignore-config", help="Ignore existing config files",
-                        action="store_true", default=False)
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s {}".format(__version__))
+    parser.add_argument(
+        "-c", "--config", nargs=1, help="Config file to load", default=None
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore-config",
+        help="Ignore existing config files",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version="%(prog)s {}".format(__version__)
+    )
     args = parser.parse_args()
 
     loadfile = args.config[0] if args.config is not None else None
@@ -32,6 +39,7 @@ def main():
     window = PyXPad(loadfile=loadfile, ignoreconfig=args.ignore_config)
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
